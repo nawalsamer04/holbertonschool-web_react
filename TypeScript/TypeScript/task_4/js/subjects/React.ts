e27006e20f425e2de13bc8b5aaf3385b727568a2
nawalsamer04 <nawalsamer04@gmail.com>
2026-05-02 03:29:10 +0300

    Create React.ts

---
+++ TypeScript/TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,14 @@
+namespace Subjects {
+  export class React extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for React';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingReact) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
